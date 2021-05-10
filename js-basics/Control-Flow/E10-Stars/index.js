8808d13ad79f2056858ab821e9219e86ee02a3fd
Mims <miriam.lennig@gmail.com>
2021-05-10 10:38:41 -0400

    code: add Exercise 10, Stars

--- js-basics/Control-Flow/E10-Stars/index.js
+++ js-basics/Control-Flow/E10-Stars/index.js
@@ -1,1 +1,14 @@
 // Control  Flow: Exercise 10 - Stars
+
+showStars(10);
+
+function showStars(rows){
+    let combinedString = '';
+    for(i = 0; i < rows; i++){
+        let combinedString = '';
+        for(j = 0; j < i + 1; j++){
+            combinedString += '*'
+        }
+        console.log(combinedString);
+    }
+}
