611660e557f8c33f6afa6fa59d80f641da99bfd5
Mims <miriam.lennig@gmail.com>
2021-05-10 09:09:16 -0400

    code: add Exercise 7, String Properties

--- js-basics/Control-Flow/E7-String-Properties/index.js
+++ js-basics/Control-Flow/E7-String-Properties/index.js
@@ -1,1 +1,26 @@
 // Control  Flow: Exercise 7 - String Properties
+
+const movie = {
+    title: 'a',
+    releaseYear: 2018,
+    rating: 4.5,
+    director: 'b'
+};
+
+showProperties2(movie);
+
+function showProperties(obj){
+    for(const [key, value] of Object.entries(movie)){
+        if(typeof(value) === "string" ){
+            console.log(`${key}: ${value}`);
+        }
+    }
+}
+
+function showProperties2(obj){
+    for(let key in obj){
+        if(typeof(obj[key]) === "string"){
+            console.log(key + ':', obj[key]);
+        }
+    }
+}
