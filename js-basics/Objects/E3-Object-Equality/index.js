c7485cda37710e8dbe13165700db830794d74f58
Mims <miriam.lennig@gmail.com>
2021-05-11 11:31:38 -0400

    Add 17- Exercise 3- Object Equality

--- js-basics/Objects/E3-Object-Equality/index.js
+++ js-basics/Objects/E3-Object-Equality/index.js
@@ -1,1 +1,27 @@
 /************ Object Equality ************/
+// Implemented using Constructor Function
+function Address(street, city, zipCode){
+    this.street = street; 
+    this.city = city;
+    this.zipCode = zipCode;     
+}
+
+let address1 = new Address('a','b','c');
+let address2 = new Address('a','b','c');
+// Points to the same address in memory
+let address3 = address1;                    
+
+// Check if contents of objects are equal
+function areEqual(address1, address2){
+    return address1.street === address2.street && 
+    address1.city === address2.city && 
+    address1.zipCode === address2.zipCode;
+}
+
+// Check if objects have the same address
+function areSame(address1, address2){
+    return address1 === address2;
+}
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
