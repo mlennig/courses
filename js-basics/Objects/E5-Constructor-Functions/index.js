2e075654f153fb28a8e5d54b3209fde5eefb0529
Mims <miriam.lennig@gmail.com>
2021-05-11 11:57:01 -0400

    Add 19- Exercise 5- Constructor Functions

--- js-basics/Objects/E5-Constructor-Functions/index.js
+++ js-basics/Objects/E5-Constructor-Functions/index.js
@@ -1,1 +1,28 @@
 /************ Constructor Functions ************/
+// Object literal syntax to initialize this object
+let blogPost = {
+    title: 'a',
+    author: 'b',
+    views: '100',
+    comments: [
+        {author:'a', body: 'b'},
+        {author:'c', body: 'd'}
+    ],
+    isLive: false
+};
+
+console.log(blogPost);
+
+// Aim to have functions with the fewest parameters possible
+function BlogPost(title, body, author){
+    this.title = title,
+    this.body = body,
+    this.author = author
+    this.views = 0;
+    this.comments = [],
+    this.isLive = false
+}
+
+let post = new BlogPost('a', 'b', 'c');
+
+console.log(post);
