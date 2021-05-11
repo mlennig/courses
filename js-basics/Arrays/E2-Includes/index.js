18c3d845779c7b9df1ede51b2ed14b49154eda1a
Mims <miriam.lennig@gmail.com>
2021-05-11 17:29:44 -0400

    code: Add 18- Exercise 2- Includes

--- js-basics/Arrays/E2-Includes/index.js
+++ js-basics/Arrays/E2-Includes/index.js
@@ -1,1 +1,17 @@
 /************ Arrays: Exercise 2 - Includes ************/
+
+const numbers = [1, 2, 3, 4];
+
+console.log(numbers.includes(1));
+
+/* Instructions */
+// Implement includes as if it doesn't already exist
+
+function includes(array, searchElement){
+    for(let number of array){
+        if(number === searchElement) return true;       
+    }
+    return false;
+}
+
+console.log(includes(numbers, 6));
