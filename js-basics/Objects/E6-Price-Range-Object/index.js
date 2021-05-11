a0159aa0a174220da34f220e7f6e7170d9adff72
Mims <miriam.lennig@gmail.com>
2021-05-11 12:24:32 -0400

    Add 20- Exercise 6- Price Range Object

--- js-basics/Objects/E6-Price-Range-Object/index.js
+++ js-basics/Objects/E6-Price-Range-Object/index.js
@@ -1,1 +1,37 @@
 /************ Price Range Object ************/
+
+// Inexpensive
+// Object literal syntax to initialize this object
+let inexpensive = {
+    priceMax: 'a',
+    selected: null
+};
+
+// Moderate
+let moderate = {
+    priceMin: 'b',
+    priceMax: 'c',
+    selected: null
+};
+
+// Pricey
+let pricey = {
+    priceMin: 'd',
+    priceMax: 'e',
+    selected: null
+};
+
+// Ultra High-End
+let ultraHighEnd = {
+    priceMin: 'f',
+    selected: null
+};
+
+let price = [inexpensive, moderate, pricey, ultraHighEnd];
+
+let priceRanges = [
+    {label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10},
+    {label: '$$', tooltip: 'Modterate', minPerPerson: 11, maxPerPerson: 20},
+    {label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50},
+]
+
