81de85ff5084378e678e3b72c71765a03013db32
Mims <miriam.lennig@gmail.com>
2021-05-11 09:23:04 -0400

    code: Add Objects: Notes - Value vs Reference thru Date

--- js-basics/Objects/index.js
+++ js-basics/Objects/index.js
@@ -1,6 +1,6 @@
 /** Notes on Objects **/
 
-//////////////////////////// Basics ////////////////////////////
+//////////////////////////// 1. Basics ////////////////////////////
 // Object-oriented Programming (OOP)
 // Group related variables
 /* const circle = {
@@ -17,26 +17,26 @@
 
 
 
-//////////////////////////// Factory Functions ////////////////////////////
+//////////////////////////// 2. Factory Functions ////////////////////////////
 // Another way to creat objects. 
 // Naming convention: Camel Case/Notation
 function createCircle(radius){
     return {
         radius,                     // When key = value, you can write it w/out the key
         draw(){
-            console.log('draw');    // Objects can be rewritten like this inside objects
+            //console.log('draw');    // Objects can be rewritten like this inside objects
         }
     };
 }
-const circle1 = createCircle(1);
+/* const circle1 = createCircle(1);
 console.log(circle1);
 
 const circle2 = createCircle(2);
 console.log(circle2);
 
-const myCircle = createCircle(1);
+const myCircle = createCircle(1); */
 
-//////////////////////////// Constructor Functions ////////////////////////////
+//////////////////////////// 3. Constructor Functions ////////////////////////////
 // Naming convention: Pascal Case/Notation
 /* This */
 // 1. The operator first creates an empty object;
@@ -52,9 +52,9 @@ function Circle(radius){
 
 //const circle = new Circle(1);   // Creates empty JS object, similar to cpp
 
-//////////////////////////// Dynamic Nature of Objects ////////////////////////////
+//////////////////////////// 4. Dynamic Nature of Objects ////////////////////////////
 // In using const, we cannot reset circle toa  new object
-const circle = {
+/* const circle = {
     radius: 1
 };
 
@@ -62,13 +62,13 @@ circle.color = 'yellow';
 circle.draw = function(){}
 
 delete circle.color;
-delete circle.draw; 
+delete circle.draw;  */
 
-//////////////////////////// Constructor Property ////////////////////////////
-const circle = createCircle(1);
-const another = new Circle(1);
+//////////////////////////// 5. Constructor Property ////////////////////////////
+/* const circle3 = createCircle(1);
+const another = new Circle(1); */
 
-let x = {};
+//let x = {};
 
 // let x = new Object();
 
@@ -76,12 +76,171 @@ new String();   // '', "", '' <-- literals
 new Boolean();  // We use true, false
 new Number();   // 1, 2, 3, ...
 
-//////////////////////////// Functions are Objects ////////////////////////////
-//////////////////////////// Value vs Reference Types ////////////////////////////
-//////////////////////////// Enumerating Properties of an Object ////////////////////////////
-//////////////////////////// Cloning an Object ////////////////////////////
-//////////////////////////// Garbage Collection ////////////////////////////
-//////////////////////////// Math ////////////////////////////
-//////////////////////////// String ////////////////////////////
-//////////////////////////// Template Literals ////////////////////////////
-//////////////////////////// Date ////////////////////////////
+//////////////////////////// 6. Functions are Objects ////////////////////////////
+/* const Circle1 = new Function('radius', `
+this.radius = radius;       
+this.draw = function(){
+    console.log('draw');
+}`); */
+
+//const circle = new Circle1(1);
+
+Circle.call({}, 1);             // Must supply an empty object as 
+                                // first arg, specifies target of 'this'
+Circle.apply({}, [1, 2, 3]);    // Instead of passing all the arguments explicitly,
+                                // we pass them in an array. 
+
+
+
+// If you don't use the new operator, this by default 
+// will point to the global object, which is window. 
+// So if I want to rewrite this expression in this way
+// instead of pass an empty objevct, I would pass window. 
+//const another = new Circle(1);
+
+//////////////////////////// 7. Value vs Reference Types ////////////////////////////
+/* Value Types */
+// Primitives
+// Number, String, Boolean, Symbol, undefined, null
+/*** Primitives are copied by their value ***/
+/*** Objects are copied by their reference ***/
+
+/* Reference Types */
+// Object, Function, Array
+
+let x = {value: 10};
+let y = x;
+
+x.value = 20;
+
+let obj = {value: 10};
+
+// Number is being passed by value as opposed to by reference 
+// when you don't pass in the object.
+// In passing an object, you're now passing by reference
+function increase(number){
+    obj.value++;
+}
+
+increase(obj);
+console.log(obj);
+
+//////////////////////////// 8. Enumerating Properties of an Object ////////////////////////////
+/* const circle = {
+    radius: 1,
+    draw(){
+        console.log('draw');
+    }
+}; */
+
+/* for(let key in circle)
+    console.log(key, circle[key]); */
+
+/* for(let key of circle)              // Can only be used with iterables
+                                    // an object is not iterable
+    console.log(key); */
+
+// Object.keys(circle)) returns a string array
+// which means we can use the for-of loop
+// Object is a built-in constructor object
+/* for(let key of Object.keys(circle))
+    console.log(key);
+
+//const x = {value:1}; // Object literal syntax
+
+// Object.entries(circle) returns each value pair as an array
+for(let entry of Object.entries(circle))
+    console.log(entry);
+
+// To see if a given property or method exists in an object
+if('radius' in circle) console.log('yes');
+if('color' in circle) console.log('yes'); */
+
+//////////////////////////// 9. Cloning an Object ////////////////////////////
+const circle = {
+    radius: 1,
+    draw(){
+        console.log('draw');
+    }
+};
+
+/* const another = {};
+for(let key in circle){
+    another[key] = circle[key];
+} */
+
+// Copies everything in circle to {}
+// Object {} doesn't have to be empty
+// Can also be used to combine multiple objects
+/* const another = Object.assign({
+    color: 'yellow'
+}, circle); */
+
+// Takes all the properties and methods in this object
+// and puts them in between the curly braces {}
+/* const another = {...circle};
+
+console.log(another); */
+
+
+//////////////////////////// 10. Garbage Collection ////////////////////////////
+// JS has a garbage collector, so we don't have to worry about this
+/* let circle = {};
+console.log(circle); */
+
+//////////////////////////// 11. Math ////////////////////////////
+// Bookmarked the mozilla JS math page
+
+
+//////////////////////////// 12. String ////////////////////////////
+// String primitive
+//const message = 'this is my first message';
+// When you use the dot operator, JS wraps the primitive
+// in a string object
+// String - JavaScript | MDN
+
+// String object
+//const another = new String('hi');
+
+//////////////////////////// 13. Template Literals ////////////////////////////
+// String primitive
+/* const message = 
+'This is my\n' +
+'\'first\' message';
+
+console.log(message); */
+
+// Object {}
+// Boolean literals true, false
+// String literals '', ""
+// Template ``
+// Because we are using ``, we can edit it the way we want it
+// to look 
+/* const another = 
+`Hi John,
+
+Thank you for joining my mailing list,. 
+
+Regards,
+Mosh`; */
+const name = 'John';
+
+// In the {} you can put mathematical expressions, variables
+// and functions
+const another = 
+`Hi ${name} ${2 + 3},
+
+Thank you for joining my mailing list,. 
+
+Regards,
+Mosh`;
+
+console.log(another);
+
+//////////////////////////// 14. Date ////////////////////////////
+const now = new Date();
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2018, 4, 11, 9);
+
+now.setFullYear(2017);
+
