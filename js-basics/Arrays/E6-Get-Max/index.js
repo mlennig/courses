d5fc972980fc37f90498aa91d3ecda39556ca9ad
Mims <miriam.lennig@gmail.com>
2021-05-12 11:33:16 -0400

    code: Add 22- Exercise 6- Get Max

--- js-basics/Arrays/E6-Get-Max/index.js
+++ js-basics/Arrays/E6-Get-Max/index.js
@@ -1,1 +1,31 @@
 /************ Arrays: Exercise 6 - Get Max ************/
+
+const numbers = [1, -6, 6, 1];
+
+const max = getMax(numbers);
+
+console.log(max);
+
+function getMax(array){
+    if(array.length === 0) return undefined;
+
+    let max = array[0];
+
+    for(i = 1; i < array.length; i++){
+        if(array[i] > max){
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+/* Challenge */
+// Implement using reduce method
+function getMax2(array){
+    if(array.length === 0) return undefined;
+
+    // If you don't pass an index for the initial value, 
+    // the index will automatically be set to the first
+    // value in the arry.
+    return array.reduce((a, b) => (a > b) ? a : b);
+}
