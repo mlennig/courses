2d2ffeb66a46f4176a499d215e7d195281efda80
Mims <miriam.lennig@gmail.com>
2021-05-13 13:10:32 -0400

    code: Add 14- Exercise 3- Error Handling
    The Ultimate JavaScript Mastery Series - Part 1 COMPLETE

--- js-basics/Functions/E3-Error-Handling/index.js
+++ js-basics/Functions/E3-Error-Handling/index.js
@@ -1,1 +1,32 @@
 //////////////////// Exercise 3 - Error Handling ////////////////////
+
+
+/* Instructions */
+// Add error-handling to our countOccurrences 
+// If the first argument is not an array, throw an exception
+// then wrap the two lines starting with 'const count' and 'console'
+// with a try-catch block. You can also wrap all three lines together
+// in a try-catch block. Catch the exception and log it on the console.
+
+try{
+const numbers = true;
+
+const count = countOccurrences2(numbers, 1);
+console.log(count);
+
+}
+catch(e){
+    alert(e);
+}
+
+function countOccurrences2(array, searchElement){
+    if(typeof array !== 'array')
+        throw new Error('Value is not an array.');
+
+    return array.reduce((accumulator, currentValue) => {
+        const occurrence = (currentValue === searchElement) ? 1 : 0;
+        console.log(accumulator, currentValue, searchElement);
+        return accumulator + occurrence;
+    }, 0);
+}
+
