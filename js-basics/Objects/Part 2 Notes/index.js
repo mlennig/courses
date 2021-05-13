c561130cfa94dad9e707c951a171654be9020cac
Mims <miriam.lennig@gmail.com>
2021-05-13 14:36:52 -0400

    doc: Add 4 pillars of OOP

---
+++ js-basics/Objects/Part 2 Notes/index.js
@@ -0,0 +1,20 @@
+/** Notes II on Objects **/
+
+/*************** 0. Four Pillars of OOP ***************/
+// Encapsulation
+// Abstraction
+// Inheritance
+// Polymorphism
+
+/*************** 1. Introduction ***************/
+/*************** 2. Object Literals ***************/
+/*************** 3. Factories ***************/
+/*************** 4. Constructors ***************/
+/*************** 5. Constructor Property ***************/
+/*************** 6. Functions are Objects ***************/
+/*************** 7. Values vs Reference Types ***************/
+/*************** 8. Adding or Removing Properties ***************/
+/*************** 9. Enumerating Properties ***************/
+/*************** 10. Abstraction ***************/
+/*************** 11. Private Properties and Methods ***************/
+/*************** 12. Getters and Setters ***************/
