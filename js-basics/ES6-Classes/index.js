7dd2441a644cd0d5ed1b62a00918944e03fe5cb9
Mims <miriam.lennig@gmail.com>
2021-05-17 11:31:14 -0400

    docs: Add headers to ES6 Classes

---
+++ js-basics/ES6-Classes/index.js
@@ -0,0 +1,10 @@
+/////////////// ES6 Classes: 12. Notes ///////////////
+/************** 1. ES6 Classes **************/ 
+/************** 2. Hoisting **************/ 
+/************** 3. Static methods **************/ 
+/************** 4. The This Keyword **************/ 
+/************** 5. Private Members Using Symbols **************/ 
+/************** 6. Private Members Using WeakMaps **************/ 
+/************** 7. Getters and Setters **************/ 
+/************** 8. Inheritance **************/ 
+/************** 9. Method Overriding **************/ 
