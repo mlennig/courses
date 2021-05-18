376a4e90d0af6bc9b7cdce8c33e5cfe3e3f69351
Mims <miriam.lennig@gmail.com>
2021-05-18 10:08:00 -0400

    code: Add ES6 Tooling Notes - 1. Modules

--- js-basics/ES6-Tooling/index.js
+++ js-basics/ES6-Tooling/index.js
@@ -1,13 +1,29 @@
 /////////////// ES6 Tooling: Notes ///////////////
 /************** 1. Modules **************/ 
+// Modules //
+// AMD = Asynchronous Module Definition 
+// Primarily used in browser applications
+// CommonJS
+// Used in Node.js
+// UMD = Universal Module Definition
+// Used both in the Browswer and in Node.js
+// ES6 Modules 
+// JS now natively supports a module format
 
-function Circle(radius){
-    this.radius = radius;
+const _radius = new WeakMap();
 
-    this.draw = function (){
-        console.log('draw');
+class Circle{
+    constructor(radius){
+        _radius.set(this, radius);
+    }
+
+    draw(){
+        console.log('Circle with radius' + _radius.get(this));
     }
 }
+
+const c = new Circle(10);
+c.draw();
 
 /************** 2. CommonJS Modules **************/ 
 /************** 3. ES6 Modules **************/ 
