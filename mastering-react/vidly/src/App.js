aa07bd8808536112585bd702c2270ac198a6f30b
Mims <miriam.lennig@gmail.com>
2021-05-20 14:25:41 -0400

    code: Add Components 15-20

---
+++ mastering-react/vidly/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Movies from "./components/movies";
+function App() {
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
