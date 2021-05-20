e75783730c1beba161bdb87184d9364c808d6e1e
Mims <miriam.lennig@gmail.com>
2021-05-20 08:24:28 -0400

    code: Add Components 11-14

--- mastering-react/counter-app/src/components/counter.jsx
+++ mastering-react/counter-app/src/components/counter.jsx
@@ -2,31 +2,48 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // State is a special property in React components.
+  // It's an object that includes any data that this
+  // component needs.
   state = {
     count: 0,
-    tags: [],
   };
 
-  renderTags() {
-    // Array is empty
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
+      // To eliminate the extra div, we can add React.Fragment
+      // Within the curly braces here we can put any valid
+      // JS expression. An expression is something that produces a value.
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    // JSX expressions are just like normal objects.
+    // You can return them from a function, you can pass
+    // them to a function, you can also sue them as a
+    // value of a constant or a variable.
+    return count === 0 ? "Zero" : count;
   }
 }
 
