73d3640a8c3ab99e4e2249d0e2d690bc863e3bb8
Mims <miriam.lennig@gmail.com>
2021-05-20 15:31:01 -0400

    code: Add Composing Components 1-4

--- mastering-react/counter-app/src/components/counter.jsx
+++ mastering-react/counter-app/src/components/counter.jsx
@@ -1,28 +1,21 @@
-// Use camelcase for components
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // State is a special property in React components.
-  // It's an object that includes any data that this
-  // component needs.
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log(this.props);
     return (
-      // To eliminate the extra div, we can add React.Fragment
-      // Within the curly braces here we can put any valid
-      // JS expression. An expression is something that produces a value.
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -33,17 +26,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    // JSX expressions are just like normal objects.
-    // You can return them from a function, you can pass
-    // them to a function, you can also sue them as a
-    // value of a constant or a variable.
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
