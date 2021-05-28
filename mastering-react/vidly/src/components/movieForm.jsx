0f7cadd8fa476d3ff61ea74fe2c18e696da5e6b7
Mims <miriam.lennig@gmail.com>
2021-05-28 13:50:43 -0400

    code: Add navigation bar and history

--- mastering-react/vidly/src/components/movieForm.jsx
+++ mastering-react/vidly/src/components/movieForm.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
-const MoveForm = () => {
-  return <h1>Move Form</h1>;
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie Form {match.params.id} </h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push("/movies")}
+      >
+        Save
+      </button>
+    </div>
+  );
 };
 
-export default MoveForm;
+export default MovieForm;
