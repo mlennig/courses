01108591630cedae075f5f49a42de38a26a40562
Mims <miriam.lennig@gmail.com>
2021-06-07 10:42:04 -0400

    code: Add error messages with input validation

--- mastering-react/vidly/src/components/loginForm.jsx
+++ mastering-react/vidly/src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,41 +8,56 @@ class LoginForm extends Component {
     errors: {},
   };
 
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
+
     const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
 
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is Required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is Required.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    return errors;
   };
 
   handeSubmit = (e) => {
-    // Prevent default behaviour of this event
-    // (full page reload)
+    // Prevent default behaviour of this event(full page reload)
     e.preventDefault();
 
     const errors = this.validate();
     console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     this.validate();
     console.log("Submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -52,6 +68,7 @@ class LoginForm extends Component {
             label="Username"
             value={account.username}
             onChange={this.handleChange}
+            error={errors.username}
           />
 
           <Input
@@ -59,6 +76,7 @@ class LoginForm extends Component {
             label="Password"
             value={account.password}
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
